cdd4f259d8a223f51f702027f71857e02f6abed2
emerson <xinayespinoza@gmail.com>
2024-06-03 07:47:20 -0500

    fix: app y requirements

--- app_google.py
+++ app_google.py
@@ -1,4 +1,3 @@
-from http import client
 from flask import Flask, Blueprint, render_template, request, redirect, url_for, flash, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc
@@ -15,11 +14,13 @@ from flask_wtf.file import FileField, FileAllowed
 from werkzeug.utils import secure_filename
 from datetime import datetime
 import re
-
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from dotenv import load_dotenv
 import os
-
+load_dotenv()
 app = Flask(__name__)
-
+app.secret_key = os.getenv('SECRET_KEY')
 
 app.secret_key = config('SECRET_KEY')  # Carga la variable SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = config(
@@ -41,7 +42,30 @@ login_manager.login_view = 'login'  # Vista de inicio de sesión
 login_manager.init_app(app)
 
 # Configura la autorización con Google API
+google_credentials = {
+    "type": os.getenv("GOOGLE_TYPE"),
+    "project_id": os.getenv("GOOGLE_PROJECT_ID"),
+    "private_key_id": os.getenv("GOOGLE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("GOOGLE_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.getenv("GOOGLE_CLIENT_EMAIL"),
+    "client_id": os.getenv("GOOGLE_CLIENT_ID"),
+    "auth_uri": os.getenv("GOOGLE_AUTH_URI"),
+    "token_uri": os.getenv("GOOGLE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("GOOGLE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("GOOGLE_CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("GOOGLE_UNIVERSE_DOMAIN")
+}
 
+# Configurar el alcance (scope) de las credenciales
+scope = ["https://spreadsheets.google.com/feeds",
+         "https://www.googleapis.com/auth/drive"]
+
+# Crear las credenciales
+creds = ServiceAccountCredentials.from_json_keyfile_dict(
+    google_credentials, scopes=scope)
+
+# Autorizar el cliente de gspread
+client = gspread.authorize(creds)
 
 # Función para verificar si la extensión del archivo es válida
 
@@ -402,7 +426,7 @@ def handle_formulario():
             [nombre, apellido, email, telefono, mensaje, fecha_envio])
         flash('Datos enviados correctamente', 'success')
     except Exception as e:
-        flash(f'Hubo un error, pronto nos conectamos: {e}', 'danger')
+        flash(f'Hubo un error: {e}', 'danger')
 
     return redirect(url_for('inicio'))
 
