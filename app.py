546c5c5d637ae56d67dce81b83f9ddc1a23ed8fd
emerson <xinayespinoza@gmail.com>
2024-06-23 15:22:46 -0500

    feat: cursos, protafolio, formulario

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ import re
 
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 app.secret_key = config('SECRET_KEY')  # Carga la variable SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = config(
